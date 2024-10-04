5aa108d20f583e4c2c17c24b2874d23a417e0e8e
Jessica Castro <jessica.castros@outlook.com>
2024-10-03 23:27:31 -0300

    chore: adjust tsconfig quantity and properties and add correct eslint

--- rotion/src/renderer/src/App.tsx
+++ rotion/src/renderer/src/App.tsx
@@ -1,5 +1,5 @@
-import './styles/global.css';
+import './styles/global.css'
 
 export const App = () => {
-  return <h1 className='text-red-400 font-bold text-4xl'>Hello, electron!</h1>
+  return <h1 className="text-red-400 font-bold text-4xl">Hello, electron!</h1>
 }
