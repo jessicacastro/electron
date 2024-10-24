b3e355bd8874e331a55bb2daedc41a8d98f6e046
Jessica Castro <jessica.castros@outlook.com>
2024-10-24 12:50:37 -0300

    chore(rotion-app): add colors, electron colors and global styles

--- rotion/src/renderer/src/App.tsx
+++ rotion/src/renderer/src/App.tsx
@@ -1,5 +1,9 @@
 import './styles/global.css'
 
 export const App = () => {
-  return <h1 className="text-violet-400 font-bold text-4xl">Hello world!</h1>
+  return (
+    <div className="h-screen w-screen bg-rotion-900 text-rotion-100">
+      <h1 className="text-4xl text-center pt-10">Página inicial</h1>
+    </div>
+  )
 }
